510de8d7d1d04d9165d80b19fc5b3617b0cd9fa3
CHanakya <chanakya.exe@gmail.com>
2024-06-02 22:46:41 +0530

    V1

---
+++ src/Components/TrackerCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TrackerCard = () => {
+  return <>
+    <div className="m-10 p-10 bg-blue-300 w-[40vw] flex items-center justify-center self-center justify-self-center">
+      <p>Version 1</p>
+    </div>
+  </>
+}
+
+export default TrackerCard  
---
+++ src/Components/index.js
@@ -0,0 +1,1 @@
+export {default as TrackerCard} from './TrackerCard.jsx';
